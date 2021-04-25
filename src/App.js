e51de8c8978737bf2e91e8286dedb1916871d226
Ulises E. Sosa <ulyses.sosa@gmail.com>
2021-04-25 22:07:44 +0200

    toogle div through button function

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ class App extends Component {
         {name: 'Ulises', age : 29},
         {name:'Carlos', age : 28},
         {name:'Stephanie', age : 31}
-    ]
+    ],
+    showPersons : false
 
   }
   changeUserNameHandler = (newName) => {
@@ -36,13 +37,35 @@ class App extends Component {
      });
  }
 
+ togglePersonHandler = () => {
+  console.log("clicked");
+  const doesShow = this.state.showPersons;
+  this.setState({showPersons: !doesShow}); 
+}
+
   render() {
+    const style = {
+      backgroundColor :' white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
     return (
+      
 
       <div className="App">
-          <Person name={this.state.users[0].name} age={this.state.users[0].age } changed={this.changeUserNameHandler} click={this.changeUserNameHardHandler.bind(this,'Max')}></Person>
+      <button 
+        style={style}
+        onClick={this.togglePersonHandler}>Swtich</button>
+       
+     { this.state.showPersons  ?
+        <div>          
+          <Person name={this.state.users[0].name} age={this.state.users[0].age } changed={this.changeUserNameHandler} click={this.changeUserNameHardHandler.bind(this,'Max')}/>
           <Person name={this.state.users[1].name} age={this.state.users[1].age } click={this.changeUserNameHardHandler.bind(this,'Louis')}>My Hooby is playing cards</Person>
           <Person name={this.state.users[2].name} age={this.state.users[2].age } click={this.changeUserNameHardHandler.bind(this,'Bob')}>And mine is Coding at night!</Person>
+      </div> : null
+      }
       </div>
     );
    
